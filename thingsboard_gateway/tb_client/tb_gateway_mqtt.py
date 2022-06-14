82d138c6309d2b49ec6d3f79eacbf639073701b5
bacnv <12888672+bacnv@users.noreply.github.com>
2022-06-14 10:01:57 +0700

    Update tb_gateway_mqtt.py

--- thingsboard_gateway/tb_client/tb_gateway_mqtt.py
+++ thingsboard_gateway/tb_client/tb_gateway_mqtt.py
@@ -32,8 +32,8 @@ log = logging.getLogger("tb_connection")
 
 
 class TBGatewayMqttClient(TBDeviceMqttClient):
-    def __init__(self, host, port, token=None, gateway=None, quality_of_service=1):
-        super().__init__(host, port, token, quality_of_service)
+    def __init__(self, host, port, token=None,pwd = None, gateway=None, quality_of_service=1):
+        super().__init__(host, port, token,pwd, quality_of_service)
         self.quality_of_service = quality_of_service
         self.__max_sub_id = 0
         self.__sub_dict = {}
